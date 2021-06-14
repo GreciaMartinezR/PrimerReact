edce20aabae37be53852f8875fd9d3d769e35447
Grecia Martinez R <gvmr.308@gmail.com>
2021-06-13 23:45:39 -0400

    Primer React con JSX

---
+++ src/componentes/saludoYlista.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+function Saludo (){
+    return(
+        <div>
+            <div>
+                <h1>Hello Dojo!</h1>
+            </div>
+            <div>
+                <h2>Thinks I need to do</h2>
+                <ol>
+                    <li>Learn React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the dogs</li>
+                </ol>
+            </div>
+        </div>
+    )
+}
+export default Saludo
+
